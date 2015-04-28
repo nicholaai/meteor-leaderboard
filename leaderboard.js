b628f8c3c5e1c8b62b9ff9eb067ce6e77e5c2abc
Nicholas Pappagalo <npappagalo@gmail.com>
2015-04-28 00:45:41 -0700

    Added sessions - highlight selected player

--- leaderboard.js
+++ leaderboard.js
@@ -1,19 +1,31 @@
+// Create a MongoDB Collection
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient) {
 
   Template.leaderboard.helpers({
     'player': function(){
+      // Retrieve all of the data from the "Playerslist" collection"
       return PlayersList.find()
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
     }    
   });
 
   Template.leaderboard.events({
     'click .player': function(){
-      console.log("You clicked a .player element");
+      var playerId = this._id;
+      // Create a session to store the unique ID of the clicked player
+      Session.set('selectedPlayer', playerId);
     }
-
   });
+
+
 }
 
 if(Meteor.isServer) {
