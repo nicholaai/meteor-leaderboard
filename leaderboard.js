a8664d59145636117f5b6fd9373c7210b0a4abbc
Nicholas Pappagalo <npappagalo@gmail.com>
2015-04-27 23:33:42 -0700

    Added leaderboard template and helper

--- leaderboard.js
+++ leaderboard.js
@@ -1,23 +1,13 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+PlayersList = new Mongo.Collection('players');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+if(Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find()
+    }    
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if(Meteor.isServer) {
+
 }
